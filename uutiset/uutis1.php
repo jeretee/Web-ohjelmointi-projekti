05c3a50b6ed46f07246ddf8e7055cc8fe79688ca
OivaM <82021204+Lenny120000@users.noreply.github.com>
2022-09-01 10:38:46 +0300

    Add files via upload
    uutiset tietokanta

---
+++ uutiset/uutis1.php
@@ -0,0 +1,7 @@
+<?php
+echo "<h1>Uusi terveydenhoitaja</h1>";
+echo "Julkaistu 1.9.2022 klo 10:35<br>";
+echo "<br>";
+echo "Terveydenhoitaja Pekka Ilmanen jatkaa 5.9.2022 alkaen.<br>";
+
+?>
---
+++ uutiset/uutis2.php
@@ -0,0 +1,6 @@
+<?php
+echo "<h1>PHP is Fun!</h1>";
+echo "Hello world!<br>";
+echo "I'm about to learn PHP!<br>";
+echo "This ", "string ", "was ", "made ", "with multiple parameters.";
+?>

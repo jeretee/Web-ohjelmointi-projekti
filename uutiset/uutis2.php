2c6eab3432b8dd50f6dc4fd78e5c03767f892036
OivaM <82021204+Lenny120000@users.noreply.github.com>
2022-09-01 10:50:09 +0300

    Update uutis2.php

--- uutiset/uutis2.php
+++ uutiset/uutis2.php
@@ -1,6 +1,9 @@
 <?php
-echo "<h1>PHP is Fun!</h1>";
-echo "Hello world!<br>";
-echo "I'm about to learn PHP!<br>";
-echo "This ", "string ", "was ", "made ", "with multiple parameters.";
+echo "<h1>Uusintakokeet</h1>
+2.9.2022 klo 9:10<br>
+<br>
+Ensi viikon uusinnat:<br>
+<br>Maanantai klo 14:00
+<br>Torstai klo 11:00
+<br>Perjantai klo 11:00";
 ?>

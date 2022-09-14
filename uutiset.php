a6441b43d34d7f1025b47aab5dbb8214f9b8ea39
OivaM <82021204+Lenny120000@users.noreply.github.com>
2022-09-14 11:17:48 +0300

    Update uutiset.php
    Nyt lukee mysql

--- uutiset.php
+++ uutiset.php
@@ -16,17 +16,34 @@ footer {
   padding: 3px;
   background-color: #474e5d;
   color: white;
+  margin-top: 350px;
 }
-
 </style>
 </head>
 <body>
 
-<?php include 'uutiset\uutis1.php';
-include 'uutiset\uutis2.php';
-include 'uutiset\uutis3.php'; ?>
+<?php 
+@ini_set("display_errors", 1);
+@ini_set("error_reporting", E_ALL);
 
+include("yhteys.php");
 
+$sql_lause =  "SELECT * FROM uutiset";
+try {
+  $kysely = $yhteys->prepare($sql_lause);
+  $kysely->execute();
+} 
+ catch (PDOException $e) {
+            die("VIRHE: " . $e->getMessage());
+       }
+$tulos = $kysely->fetchAll();
+
+foreach($tulos as $rivi) { 
+ echo '<h1>',$rivi['otsikko'],'</h1>';
+ echo '<br>',$rivi['aika'],'</br>';
+ echo '<p>',$rivi['leipateksti'],'</p>';
+} 
+?>  
 
 <footer>
     <h2>Copyright: Jere, Oiva, Kasper</h2>

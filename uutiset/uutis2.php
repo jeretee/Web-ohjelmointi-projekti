0b6b634d6b79d2ece25f79c319f84feb687d9ded
OivaM <82021204+Lenny120000@users.noreply.github.com>
2022-09-14 11:18:24 +0300

    Delete uutiset directory

--- uutiset/uutis2.php
+++
@@ -1,9 +0,0 @@
-<?php
-echo "<h1>Uusintakokeet</h1>
-2.9.2022 klo 9:10<br>
-<br>
-Ensi viikon uusinnat:<br>
-<br>Maanantai klo 14:00
-<br>Torstai klo 11:00
-<br>Perjantai klo 11:00";
-?>
--- uutiset/uutis3.php
+++
@@ -1,6 +0,0 @@
-<?php
-echo "<h1>Koulun palvelin pois käytöstä</h1>
-2.9.2022 klo 9:15<br>
-<br>
-<br>Koulun palvelin sammutetaan 6.9.2022, muistakaa tallentaa työt!";
-?>

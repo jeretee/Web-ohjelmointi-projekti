66ce15632d709712b6cd40d1a1241dd37f2ea9ff
OivaM <82021204+Lenny120000@users.noreply.github.com>
2022-09-15 10:05:22 +0300

    Update ilmoittautuminen.php
    korjattu

--- ilmoittautuminen.php
+++ ilmoittautuminen.php
@@ -16,26 +16,54 @@ footer {
   padding: 3px;
   background-color: #474e5d;
   color: white;
-  margin-top: 350px;
+  margin-top: 650px;
 }
-.header {
-  padding: 40px;
-  text-align: center;
-  background: #2a4742;
-  color: white;
-  font-size: 30px;
-}
+
 div	{
 	padding-left:20px;
+}
+
+.header {
+  padding: 15px;
+  text-align: left;
+  background: #2a4742;
+  color: white;
+  font-size: 20px;
+}
+
+.header-right {
+ float: right;
+ color: white;
+}
+
+.header a {
+ text-decoration:none;
+ padding: 10px;
+ border-radius: 4px;
+ line-height: 10px;
+}
+
+.header-right a:hover {
+  background-color: #ddd;
+  color: black;
+}
+
+a:visited {
+ color: white;
 }
 </style>
 </head>
 <body>
-	
+
 <div class="header">
-  <h2>Amis Padel</h2>
+  <a style="font-weight: bold;">Amis Padel</a>
+  <div class="header-right">
+    <a href="etusivu.html">Etusivu</a>
+    <a href="ilmoittautuminen.html">Ilmoittautuminen</a>
+    <a href="uutiset.php">Uutiset</a>
+  </div>
 </div>
-	
+
 <div>
 <?php 
 @ini_set("display_errors", 1);
@@ -43,6 +71,18 @@ div	{
 
 include("yhteys.php");
 
+$et = $_POST["etunimi"];
+$sn = $_POST["sukunimi"];
+$sp = $_POST["sahkoposti"];
+$kt = $_POST["koulutalo"];
+$vs = $_POST["viesti"];
+$ks = $_POST["kurssi"];
+
+echo "<br>";
+echo "Tiedot lähetetty!<br>";
+
+$sql = "INSERT INTO jasenet (tunniste, etunimi, sukunimi, sahkoposti, koulutalo, viesti, kurssi) 
+VALUES (NULL, '$et', '$sn', '$sp', '$kt', '$vs', '$ks')";
 $sql_lause =  "SELECT * FROM uutiset";
 try {
   $kysely = $yhteys->prepare($sql_lause);
@@ -51,13 +91,13 @@ try {
  catch (PDOException $e) {
             die("VIRHE: " . $e->getMessage());
        }
-$tulos = $kysely->fetchAll();
 
-foreach($tulos as $rivi) { 
- echo '<h1>',$rivi['otsikko'],'</h1>';
- echo '<br>',$rivi['aika'],'</br>';
- echo '<p>',$rivi['leipateksti'],'</p>';
-} 
+try { 
+		$kysely = $yhteys->prepare($sql); 
+		$kysely->execute(); 
+	} catch (PDOException $e) { 
+	die("VIRHE NRO 2: " . $e->getMessage()); 
+	}
 ?>  
 </div>
 

ad7672cda8249cd098e6166e6b967fcd9fdea384
OivaM <82021204+Lenny120000@users.noreply.github.com>
2022-09-15 08:49:24 +0300

    Update ilmoittautuminen.php

--- ilmoittautuminen.php
+++ ilmoittautuminen.php
@@ -1,26 +1,58 @@
-<?php 
+<!DOCTYPE html>
+<html lang="fi">
+<head>
+<meta charset="utf-8"/>
+<title>Uutiset</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+body, html {
+  height: 100%;
+  margin: 0;
+  font-family: Arial, Helvetica, sans-serif;
+}
 
+footer {
+  text-align: center;
+  padding: 3px;
+  background-color: #474e5d;
+  color: white;
+  margin-top: 350px;
+}
+div	{
+	padding-left:20px;
+}
+</style>
+</head>
+<body>
+
+<div>
+<?php 
 @ini_set("display_errors", 1);
 @ini_set("error_reporting", E_ALL);
 
 include("yhteys.php");
 
-$et = $_POST["etunimi"];
-$sn = $_POST["sukunimi"];
-$sp = $_POST["sahkoposti"];
-$kt = $_POST["koulutalo"];
-$vs = $_POST["viesti"];
-$ks = $_POST["kurssi"];
+$sql_lause =  "SELECT * FROM uutiset";
+try {
+  $kysely = $yhteys->prepare($sql_lause);
+  $kysely->execute();
+} 
+ catch (PDOException $e) {
+            die("VIRHE: " . $e->getMessage());
+       }
+$tulos = $kysely->fetchAll();
 
-echo "$et .. $sn .. $sp .. $kt .. $vs .. $ks<br>";
-
-$sql = "INSERT INTO jasenet (tunniste, etunimi, sukunimi, sahkoposti, koulutalo, viesti, kurssi) 
-VALUES (NULL, '$et', '$sn', '$sp', '$kt', '$vs', '$ks')";
-
-try { 
-		$kysely = $yhteys->prepare($sql); 
-		$kysely->execute(); 
-	} catch (PDOException $e) { 
-	die("VIRHE NRO 2: " . $e->getMessage()); 
+foreach($tulos as $rivi) { 
+ echo '<h1>',$rivi['otsikko'],'</h1>';
+ echo '<br>',$rivi['aika'],'</br>';
+ echo '<p>',$rivi['leipateksti'],'</p>';
 } 
 ?>  
+</div>
+
+<footer>
+    <h2>Copyright: Jere, Oiva, Kasper</h2>
+</footer>
+
+</body>
+</html>
